907dd5017a383b692fd9814e39082d10800a96d4
Ivan Pereira <navi1921@gmail.com>
2020-05-18 19:46:16 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -8,9 +8,10 @@ import pickle
 import context
 
 #re_model = ClassificationModel('roberta', '/content/drive/My Drive/NLP/RE_models', args={})
-re_model = ClassificationModel('roberta', 'RE_models', use_cuda=True)
+re_model = ClassificationModel('roberta', 'RE_models', use_cuda=False)
 entities_dict = pickle.load( open( "entities_dict.p", "rb" ) )
 
+interest_var_dict={'movie_value':'movie'}
 login = "dslab"
 password = "DSLABm3d3ixa3ntrar" 
 
@@ -49,7 +50,7 @@ def remove_duplicated_relations(relations):
   return filtered_relations
 
 
-def extend_triples(tuples):
+def extend_triples(tuples,entities,uris):
   new_tuples=[]
   for triple in tuples:
     if(triple[2].startswith('genre_')):
@@ -70,59 +71,112 @@ def extend_triples(tuples):
         if(triple[0] in constants.PERSON or triple[0] in constants.MOVIE_SERIE ):
           new_tuples.append((triple[0],'has_award','award'))
     else:
-      if(triple[0] in constants.PERSON):
+      if(triple[0] == 'person'):
+        for uri in uris:
+          new_tuples.append(('person', 'has_value', uri[0]))
+
+      elif(triple[0] in constants.PERSON):
         cond=False
         for t in constants.TRIPLES_PERSON:
           if(triple[2]==t[2]):
             cond=True
             new_tuples.append((triple[0], t[1], triple[2]))
         if(cond==False):
-          uris = get_person_uri(triple[2])
+          #uris = get_person_uri(triple[2])
           for uri in uris:
             if(triple[0].lower()==uri[1].lower()):
               new_tuples.append((triple[0], 'has_value', uri[0]))
-      if(triple[0] in constants.MOVIE_SERIE):
+      elif(triple[0] in constants.MOVIE_SERIE):
         cond=False
         for t in constants.TRIPLES_MOVIE:
           if(triple[2]==t[2]):
             cond=True
             new_tuples.append((triple[0], t[1], triple[2]))
         if(cond==False):
-          uris = get_uri_from_movie_serie(triple[2])
-          print('uris: ')
-          print(uris)
-          if(triple[0].lower()==uris[0].lower()):
-            #Estou colocando todas as uris com esse titulo
+          #we have already found uris from movie
+          if(len(uris)>0):
+            #uris = get_uri_from_movie_serie(triple[2])
+            print('movies uris: ')
+            print(uris)
+            #if(triple[0].lower()==uris[0].lower()):
+              #print(uris)Estou colocando todas as uris com esse titulo
+            for uri in uris:
+              new_tuples.append((triple[0], 'has_value', uri[0]))
+          else:
+            print('finding uris in extend_triples')
+            uris = get_uri_from_movie_serie(triple[2])
+            print(uris)
             for uri in uris[1]:
               new_tuples.append((triple[0], 'has_value', uri))
-        
+          
+
       else:
         #identificar como titulo de filme,nome de empresa, ou nome de pessoa
+          #bugfix: ajustar entity com  o titulo do filme
+          for ent in entities:
+            if(ent['entity'] == triple[0]):
+              ent['entity'] = 'movie'
 
-        #checar nome pessoa
-        uris = get_person_uri(triple[0])
-        print('uris: ',uris)
-        if(len(uris[0])>=1):
-          #é pessoa!
-          for t in constants.TRIPLES_PERSON:
-            if(triple[2]==t[2]):
-              new_tuples.append(('person', t[1], triple[2]))
-          new_tuples.append(('person', 'has_value', uris[0][0]))
-        
-        new_tuples.append(('person', 'has_value', uris[0][0]))
+        #a=2/0
+        #new_tuples.append(('person', 'has_value', uris[0][0]))
 
   return new_tuples
   
+def is_entity(entity):
+  if entity in constants.NER_ENTITIES_MAP:
+    return True
+  if(entity  in constants.GENRE_MAP):
+    return True
+  
+  
+  return False
+
 
 def construct_tuples(relations,entities):
   ent_dict = {}
+  tuples_relation =[]
+  uris = []
+
+  values_dict = {}
+
+  for ent in entities:
+    if(ent['entity'] == 'people_names'):
+      #checar nome pessoa
+      uris = get_person_uri(ent['value'])
+      
+      #if(len(uris)>=1):
+      tuples_relation.append(('person','',ent['value']))
+      values_dict[ent['value']] = 'person'
+        #é pessoa!
+        #for uri in uris:
+        #  tuples_relation.append((uri[1],'', ent['value'] ))
+
+    elif(not is_entity(ent['entity'])):
+      print('checking what entity is')
+      print(ent['entity'])
+      #checar nome pessoa
+      uris = get_person_uri(ent['entity'])
+      
+      if(len(uris)>=1):
+        #é pessoa!
+        for uri in uris:
+          tuples_relation.append((uri[1],'', ent['value'] ))
+          values_dict[ent['value']] = 'person'
+      
+      #checar titulo filme
+      uris = get_uri_from_movie_serie(ent['entity'])
+      if(len(uris[1])>=1):
+        print('uris: ',uris)
+        ent['entity']=uris[0]
+        tuples_relation.append((uris[0], '', ent['value']))
+        
   for ent in entities:
     if(ent['entity']=='people_names'):
       pass 
     else:
       ent_dict[ent['value']] = ent['entity']
   
-  tuples_relation =[]
+  
   for rel in relations:
     left_val = rel.split('|')[1]
     right_val = rel.split('|')[2]
@@ -134,9 +188,14 @@ def construct_tuples(relations,entities):
       obj = ent_dict[right_val]
     else:
       obj = right_val
+
+    #substitui titulos/person name nas relacoes pela entidade
+    if obj in values_dict:
+      obj = values_dict[obj]
+
     tuples_relation.append([subj,"",obj])
   
-  return tuples_relation
+  return tuples_relation,uris
 
 
 def run_sparql(sparql_query):
@@ -253,9 +312,28 @@ def search(text=''):
               relations.append([ent['entity'],'',interest_entity['entity']])
       print(context_entities)
       print('infered relations:',relations)
-    
+
+    else:
+      relations=[]
+      cond=True
+      for hist in cont.history:
+        for ent in hist[1]:
+          for interest_entity in interest_entities:
+            if is_domain(interest_entity['entity'],ent['entity'],cont):
+              if(cond):
+                print(hist)
+                #print(hist[0]) 
+                interest_var=hist[0][0]
+                #print(hist[3]['results']) 
+                results=hist[3]['results'][interest_var]
+                context_interest_var = interest_var_dict[interest_var]
+                relations.append([context_interest_var,'',results[ref]])
+                relations.append([context_interest_var,'',interest_entity['entity']])
+                print('relations: ',relations)
+                cond=False
+
     raw_relations_tuples = copy.deepcopy(relations)
-    relations_tuples =extend_triples(relations)
+    relations_tuples =extend_triples(relations,entities,[])
     print('tuples after: ',relations_tuples)
 
     rec_relations = relation_recommendation(relations_tuples)
@@ -264,27 +342,37 @@ def search(text=''):
     sparql_query,interest_var = sparql_build(relations_tuples)
     print(sparql_query,interest_var)
 
-    results = run_sparql(sparql_query)
-    print(results)
+    try:
+      results = run_sparql(sparql_query)
+      print(results)
+      data= encode([results],rec_relations)
+      print(data)
 
-    data= encode([results],rec_relations)
-    print(data)
+      cont.set_current_turn_results(text,data,entities_rasa['intent']['name'],entities,raw_relations_tuples,results['head']['vars'])
+      return data
+    except:
+      output = {
+        "text": "Sem resultados.",
+        "related": [],
+        "relations":[],
+        "eval_options": True
+        }
+      return output
 
-    cont.set_current_turn_results(text,data,entities_rasa['intent']['name'],entities,raw_relations_tuples,results['head']['vars'])
-
-    return data
-  
   queries = get_relations_queries(entities_rasa)
   print(queries)
   
   pred_relations=relation_extraction(queries)
   print(pred_relations)
   
-  relations_tuples=construct_tuples(pred_relations,entities)
+  relations_tuples,uris=construct_tuples(pred_relations,entities)
   print('tuples before: ',relations_tuples)
+  print('Correct entities: ')
+  pprint(entities)
+  
   raw_relations_tuples = copy.deepcopy(relations_tuples)
 
-  relations_tuples =extend_triples(relations_tuples)
+  relations_tuples =extend_triples(relations_tuples,entities,uris)
   print('tuples after: ',relations_tuples)
 
   rec_relations = relation_recommendation(relations_tuples)
@@ -293,14 +381,50 @@ def search(text=''):
   sparql_query,interest_var = sparql_build(relations_tuples)
   print(sparql_query,interest_var)
   #return entities_rasa['intent']['name'],entities,relations_tuples,interest_var
-
-  results = run_sparql(sparql_query)
-  print(results)
-
-  data= encode([results],rec_relations)
-  print(data)
-
-  cont.set_current_turn_results(text,data,entities_rasa['intent']['name'],entities,raw_relations_tuples,results['head']['vars'])
-
   
-  return data
+  try:
+    results = run_sparql(sparql_query)
+    print(results)
+
+    data= encode([results],rec_relations)
+    print(data)
+
+    cont.set_current_turn_results(text,data,entities_rasa['intent']['name'],entities,raw_relations_tuples,results['head']['vars'])
+    return data
+  except:
+    output = {
+        "text": "Sem resultados.",
+        "related": [],
+        "relations":[],
+        "eval_options": True
+        }
+    return output
+    
+
+#text = 'Indicações do filme El Sistema Pelegrin'
+#text='O filme The Vampire obteve que premiação?'
+#text = 'O Ator Geraldo Rivera do filme Volver ganhou o que.'
+#text = 'me diga a premiacao da atriz Angelina Jolie'
+#text = 'me diga a premiacao do Geraldo Rivera'
+#text = 'atores que ganharam o oscar'
+#text = 'atores que foram indicados ao oscar'
+text = 'filmes que ganharam o oscar'
+#text= 'premios do avatar'
+text = 'Me diga filmes da Angelina Jolie'
+#text = 'Me diga filmes da atriz Angelina Jolie'
+
+#text = 'Me mande filmes de crime'
+
+results = search(text)
+print(results)
+text = 'atores desse primeiro'
+#text = 'suas atrizes'
+
+results = search(text)
+print(results)
+
+#Atores de avatar
+#suas atrizes (avatar)
+#seus premios (avatar)
+#premio do primeiro (atrizes)
+#preimio do primeiro ator (ator)
