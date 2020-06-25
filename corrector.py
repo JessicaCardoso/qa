91013dd1b6e8b089ce0051922fb29487208b403c
Ivan Pereira <navi1921@gmail.com>
2020-06-25 17:42:59 -0300

    Update corrector.py

--- corrector.py
+++ corrector.py
@@ -6,9 +6,31 @@ inv_areas = pickle.load(open('corrector/inv_areas.b','rb'))
 count_vect = pickle.load(open('corrector/count_vect.b','rb'))
 entities_dict = pickle.load(open('entities_dict.p','rb'))
 
-movie_data = pickle.load(open('pandas_data/movies_df.dat','rb'))
-people_data = pickle.load(open('pandas_data/person_df.dat','rb'))
+movie_data=[]
+people_data=[]
+
+movie_data = pickle.load(open('pandas_data/movies_titles.p','rb'))
+people_data = pickle.load(open('pandas_data/person_names.p','rb'))
+
 print("Corrector data loaded!!!")
+"""
+names=[]
+for index, row in people_data.iterrows():
+    name = row['birthName']
+    if ',' in name:
+      n = name.split(',')
+      name = n[1][1:]+' '+n[0]
+    names.append(name)
+
+people_data=[]
+titles=[]
+for index, row in movie_data.iterrows():
+    titles.append(row['title'])
+movie_data=[]
+
+pickle.dump(names,open('pandas_data/person_names.p','wb'))
+pickle.dump(titles,open('pandas_data/movies_titles.p','wb'))
+"""
 
 def get_suggestion_property(text):
   x = count_vect.transform(text)
@@ -28,27 +50,16 @@ def filter_results(res):
     f_res[res[i]]=1
   return f_res.keys()
 
+
 def get_correct_title(text):
-  titles =[]
-  bindings = movie_data['results']['bindings']
-  for b in bindings:
-    title = b['title']['value']
-    titles.append(title)
-  titles_results = difflib.get_close_matches(text, titles,n=4,cutoff=0.7)
+  titles_results = difflib.get_close_matches(text, movie_data,n=4,cutoff=0.7)
   return filter_results(titles_results)
 
 def get_correct_people_name(text):
-  names =[]
-  bindings = people_data['results']['bindings']
-  for b in bindings:
-    name = b['birthName']['value']
-    if ',' in name:
-      n = name.split(',')
-      name = n[1][1:]+' '+n[0]
-    names.append(name)
-  names_results = difflib.get_close_matches(text, names,n=4,cutoff=0.7)
+  print('Searching with difflib...')
+  names_results = difflib.get_close_matches(text, people_data,n=4,cutoff=0.7)
+  print('returning results!')
   return filter_results(names_results)
-
 
  
 #s=get_suggestion_property(['prmio'])
